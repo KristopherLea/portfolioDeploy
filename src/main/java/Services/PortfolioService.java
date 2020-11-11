a5ff1ebc035887465c5714ad250d58f1e7ea9272
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-11 14:39:01 -0600

    modded portfolio services

--- src/main/java/Services/PortfolioService.java
+++ src/main/java/Services/PortfolioService.java
@@ -4,4 +4,6 @@ import Models.Portfolio;
 
 public interface PortfolioService {
 public Portfolio getPortfolio();
+public void postPortfolio(Portfolio portfolio);
+public Iterable<Portfolio> getAllPortfolios();
 }
--- src/main/java/Services/PortfolioServiceImpl.java
+++ src/main/java/Services/PortfolioServiceImpl.java
@@ -1,2 +1,31 @@
-package Services;public class PortfolioServiceImpl {
+package Services;
+
+
+import Models.Portfolio;
+import Repositories.PortfolioRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class PortfolioServiceImpl implements PortfolioService{
+
+    @Autowired
+    PortfolioRepo portfolioRepo;
+
+    @Override
+    public Portfolio getPortfolio(){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("https://dog.ceo/api/breeds/image/random",Portfolio.class);
+    }
+
+    @Override
+    public void postPortfolio(Portfolio portfolio){
+        portfolioRepo.save(portfolio);
+    }
+
+    @Override
+    public Iterable<Portfolio> getAllPortfolios(){
+        return portfolioRepo.findAll();
+    }
 }
