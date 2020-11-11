9ccf00d6719cac48a4a80069a239e7bdf916bfc1
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-11 13:26:47 -0600

    adds react files

--- src/main/java/com/example/portfolioDeploy/PortfolioDeployApplication.java
+++ src/main/java/com/example/portfolioDeploy/PortfolioDeployApplication.java
@@ -2,6 +2,9 @@ package com.example.portfolioDeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioDeployApplication {
@@ -10,4 +13,13 @@ public class PortfolioDeployApplication {
 		SpringApplication.run(PortfolioDeployApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://localhost:4200");
+			}
+		};
+	}
 }
